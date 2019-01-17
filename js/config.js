5ddd50f6f78e57562d68627db39fa312e3997ec7
Ganjiguur N <a.alua07@gmail.com>
2019-01-17 14:55:58 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_iGCEb9TYd', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '3590e7m5ab459s4is25ol0it46' //is this used anywhere?
+    },
+};
+
